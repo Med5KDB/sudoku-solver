62b5be3a5ed97fd4fbbfcc0f9273c927622c531e
Med5KDB <kevindebruyne17med@gmail.com>
2025-03-24 07:55:34 +0000

    add error handling for file reading in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,34 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int[][] mat;
         Scanner scanner = new Scanner(System.in);
 
-        if (args.length > 0) {
+        try {
+            if (args.length > 0) {
+                String filename = args[0];
+                mat = FileReader.readBoardFromFile(filename);
+            } else {
+                System.out.println("Aucun fichier fourni en argument. Veuillez saisir la grille manuellement");
+                mat = UserInputReader.readBoardFromUser(scanner);
+            }
 
-            String filename = args[0];
-            mat = FileReader.readBoardFromFile(filename);
-        } else {
-            System.out.println("Aucun fichier fourni en argument. Veuillez saisir la grille manuellement");
-            mat = UserInputReader.readBoardFromUser(scanner);
-        }
-        Sudoku sudoku = new Sudoku();
-        if (sudoku.isValid(mat, 0)) {
-            System.out.println("Solution trouvée :");
-            // sudoku.printBoard(mat);
-            BoardPrinter.printBoard(mat);
-        } else {
-            System.out.println("Pas de solution possible !");
+            Sudoku sudoku = new Sudoku();
+            if (sudoku.isValid(mat, 0)) {
+                System.out.println("Solution trouvée :");
+                BoardPrinter.printBoard(mat);
+            } else {
+                System.out.println("Pas de solution possible !");
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Erreur: Fichier introuvable.");
+        }catch (IOException e) {
+            System.err.println("Erreur de lecture du fichier: " + e.getMessage());
+        } finally {
+            scanner.close();
         }
     }
-
 }
